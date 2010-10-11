8df027f13ffe2c5927ec2afeb1c811c77d2e0da1
jenz <jenz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-11 17:37:33 +0000

    Added license info
    git-svn-id: https://plugins.svn.wordpress.org/sdac-translate/trunk@299005 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sdac-translate.php
+++ sdac-translate.php
@@ -6,6 +6,17 @@ Description: Offer simple and lightweight site translation using <a href="http:/
 Author: Jennifer Zelazny/SDAC Inc.
 Version: 1.1
 Author URI: http://www.sandboxdev.com/
+---------------------------------------------------
+Released under the GPL license
+http://www.opensource.org/licenses/gpl-license.php
+---------------------------------------------------
+This is an add-on for WordPress
+http://wordpress.org/
+---------------------------------------------------
+This plugin is distributed  WITHOUT ANY WARRANTY; 
+without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+---------------------------------------------------
 */
 
 
